56935f16f525e4343f7a8258abd95e62d3557334
sasha <sssaaasha@mail.ru>
2023-02-15 03:11:18 +0300

    add Fetch

--- js/script.js
+++ js/script.js
@@ -224,32 +224,39 @@ window.addEventListener('DOMContentLoaded', () =>{
             statusMessage.textContent = message.loading;
             form.append(statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-
-            request.setRequestHeader('Content-type', 'application/json');
+            
+            
             const formData = new FormData(form);
 
             const object = {};
             formData.forEach(function(value, key) {
                 object[key] = value;
             });
-            const json = JSON.stringify(object);
+           
             
 
-            request.send(json);
-            request.addEventListener('load', () => {
-                if(request.status === 200) {
-                    console.log(request.response);
-                    statusMessage.textContent = message.success;
-                    form.reset();
-                    setTimeout(() => {
-                        statusMessage.remove();
-                    }, 2000);
-                } else {
-                    statusMessage.textContent = message.failure;
-                }
+            fetch('server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'    
+                },
+                body:  JSON.stringify(object)
+            }).then(data => data.text())
+            .then(data => {
+                console.log(data);
+                statusMessage.textContent = message.success;
+                
+            }).catch(() => {
+                statusMessage.textContent = message.failure;
+            }).finally(() => {
+                form.reset();
+                setTimeout(() => {
+                statusMessage.remove();
+                }, 2000); 
             });
+
+            
+           
 
         });
     }
